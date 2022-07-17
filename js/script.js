93ca01db9ce96d2e4296ed47c1b2f04bbf02cd47
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-07-17 20:46:36 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -29,5 +29,5 @@ x[i].style.display = "none";
 myIndex++;
 if (myIndex > x.length) {myIndex = 1}    
 x[myIndex-1].style.display = "block";  
-setTimeout(carousel, 2000);
+setTimeout(carousel, 3000);
 }
